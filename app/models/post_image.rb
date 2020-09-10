224dc6eabe9c0e31c6709c6adb148e68f1bbbe70
EC2 Default User <ec2-user@ip-172-31-67-151.ec2.internal>
2020-09-10 03:36:28 +0000

    [Add] comment_destroy

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,7 +2,6 @@ class PostImage < ApplicationRecord
 
   belongs_to :user
   attachment :image
-  
-  has_many :post_commens, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_092426) do
+ActiveRecord::Schema.define(version: 2020_09_09_103346) do
 
   create_table "post_coments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
     t.integer "post_image_id"
